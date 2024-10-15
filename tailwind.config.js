259ab23bd97ec3532137f5a4b6148a524af09c58
supto <salatussadik131@gmail.com>
2024-10-16 01:50:52 +0530

    banner 3 complite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}","./*.{html,js}"],
   theme: {
     extend: {
+      colors:{
+        heading:"#494E59",
+        text:"#6A7989",
+      },
       fontFamily: {
         sans: ['Nunito Sans', 'sans-serif'], // Override the default sans font
       },
