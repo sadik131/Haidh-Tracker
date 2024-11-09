58762fedd2cf8ea682d8fae0d75d9939cb39ec0f
supto <salatussadik131@gmail.com>
2024-11-09 15:03:32 +0530

    add container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","./*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
-    
+
+    container: {
+      padding: {
+        DEFAULT: "0px",
+      },
+    },
     extend: {
-      colors:{
-        heading:"#494E59",
-        text:"#6A7989",
-        bgColor:"#494E59",
-        contentTextColor:"#75A2E9"
+      colors: {
+        heading: "#494E59",
+        text: "#6A7989",
+        bgColor: "#494E59",
+        contentTextColor: "#75A2E9"
       },
       fontFamily: {
         sans: ['Noto Serif SC', 'serif'],
         nunito: ["Nunito Sans", 'sans-serif'],
-        
       },
+      screens: {
+        maxWidth: {
+          sm: "100%",
+          md: "768px",
+          lg: "968px",
+          xl: "1200px",
+          "2xl": "1920px",
+        },
+      }
     },
   },
   plugins: [],
